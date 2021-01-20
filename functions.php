51ebb06caf5af03b10943d0c0bba29e3c671ec87
Andrew Rhyand <andrew.rhyand@gmail.com>
2021-01-20 13:42:28 -0800

    add 'movie' cpt to theme

--- functions.php
+++ functions.php
@@ -6,5 +6,8 @@ require get_stylesheet_directory() . '/src/helpers.php';
 // WP actions and filters setup
 require get_stylesheet_directory() . '/src/wp-hooks.php';
 
+// Register 'movie' custom post type
+require get_stylesheet_directory() . '/src/movie-cpt.php';
+
 // Custom REST API endpoints
 require get_stylesheet_directory() . '/src/rest-endpoints.php';
